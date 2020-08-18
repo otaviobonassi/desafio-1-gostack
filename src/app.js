d0eea62549f04dcd9f1cae18993dc7b21fed44f0
Otavio Bonassi <obonassi@gmail.com>
2020-08-17 22:30:03 -0300

    solving challenge 1

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,51 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+
+  const newObj = {id: uuid(), title, url, techs, likes: 0};
+  repositories.push(newObj);
+
+  return response.status(201).json(newObj);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  const {id} = request.params;
+
+  const itemIndex = repositories.findIndex(item => item.id === id);
+
+  if(itemIndex < 0) return response.status(400).json({message: "Item não encontrado"});
+
+  repositories[itemIndex] = {...repositories[itemIndex], title, url, techs};
+  return response.status(200).json(repositories[itemIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const itemIndex = repositories.findIndex(item => item.id === id);
+  
+  if(itemIndex < 0) return response.status(400).json({message: "Item não encontrado"});
+
+  repositories.splice(itemIndex, 1);
+  
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const itemIndex = repositories.findIndex(item => item.id === id);
+  if(itemIndex < 0) return response.status(400).json({message: "Item não encontrado"});
+  const prevLikes = repositories[itemIndex].likes;
+  repositories[itemIndex] = {...repositories[itemIndex], likes: prevLikes + 1};
+
+  return response.status(200).json(repositories[itemIndex]);
 });
 
 module.exports = app;
